2b26314cccb0649b67ce1c33c651c3848f78de3f
rusadi-0 <hadirusadi97@gmail.com>
2023-10-25 11:54:47 +0800

    work at office

--- assets/sql/kasir.sql
+++ assets/sql/kasir.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 21, 2023 at 11:23 AM
+-- Generation Time: Oct 25, 2023 at 03:53 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
@@ -33,7 +33,7 @@ CREATE TABLE `produk` (
   `nama` varchar(255) NOT NULL,
   `harga` int(25) NOT NULL,
   `satuan` varchar(8) NOT NULL,
-  `stok` int(10) DEFAULT NULL
+  `stok` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -41,10 +41,52 @@ CREATE TABLE `produk` (
 --
 
 INSERT INTO `produk` (`id`, `barcode`, `nama`, `harga`, `satuan`, `stok`) VALUES
-(1, '2', 'susu', 10000, 'pcs', 3),
-(2, '3', 'sarden', 6570, 'pcs', 5),
-(3, '4', 'miehyfjhgfhfhfhfhgfhfhfhgfhgfhfhg', 2760, 'pcs', 4),
-(4, '8997018460150', 'MARLBORO ICE BLAST 20', 36510, 'pcs', 2);
+(1, '2', 'anjing', 5000, 'pcs', 999932),
+(2, '3', 'MIE GORENG', 2850, 'pcs', 0),
+(3, '4', 'amsil gelas', 20100, 'pcs', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `waktu` int(255) NOT NULL,
+  `total` int(255) NOT NULL,
+  `kembalian` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `waktu`, `total`, `kembalian`) VALUES
+(54, 1698152936, 31000, 0),
+(55, 1698152978, 61500, 38500),
+(56, 1698154122, 16500, 3500),
+(57, 1698154866, 16500, 3500),
+(58, 1698154933, 27500, 972500),
+(59, 1698155039, 49500, 500),
+(60, 1698155043, 49500, 500),
+(61, 1698155043, 49500, 500),
+(62, 1698155043, 49500, 500),
+(63, 1698155044, 49500, 500),
+(64, 1698155044, 49500, 500),
+(65, 1698155044, 49500, 500),
+(66, 1698155180, 16500, 6833),
+(67, 1698155551, 16500, 3500),
+(68, 1698155624, 44000, 6000),
+(69, 1698155640, 22000, 28000),
+(70, 1698156467, 22000, 18000),
+(71, 1698156630, 5500, 34500),
+(72, 1698156811, 49500, 500),
+(73, 1698157301, 33000, 17000),
+(74, 1698157356, 33000, 17000),
+(75, 1698157708, 5500, 54500),
+(76, 1698158056, 5500, 84500),
+(77, 1698198606, 22000, 28000);
 
 --
 -- Indexes for dumped tables
@@ -57,6 +99,12 @@ ALTER TABLE `produk`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -65,6 +113,12 @@ ALTER TABLE `produk`
 --
 ALTER TABLE `produk`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
